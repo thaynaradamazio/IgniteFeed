c107a709aa3010474b60d7fd2a69392ef02cd512
thaynaradamazio <thaydamazio2015@gmail.com>
2025-04-25 10:57:47 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+    <Post
+      author="thaynara"
+      content="ufjsdhfcoiusdnfdgfkjsdbf" 
+    />
+    <Post
+      author="damazio"
+      content="dsjnhoisdfnd" 
+    />
+    </div>
+  )
+}
+
